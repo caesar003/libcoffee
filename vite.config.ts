02a174a70be2d921678a6561fad277c7f0590684
caesar003 <caesarmuksid@gmail.com>
2024-08-17 18:07:08 +0700

    intial-commit

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@constants": path.resolve(__dirname, "src/constants"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@interfaces": path.resolve(__dirname, "src/interfaces"),
+      "@services": path.resolve(__dirname, "src/services"),
+    },
+  },
+});
